85c07b92d385930203bb57d472e403f287d57bdc
Reshma <reshukakkirala@gmail.com>
2026-07-03 12:34:52 +0530

    Added Express server and frontend

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3002;
+
+app.use(express.static("frontend"));
+
+app.listen(PORT, () => {
+    console.log("Server running at http://localhost:3002");
+});
